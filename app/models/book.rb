0c9eccf5678bee67fc7ab0cc8b2b1023faab85c1
keitaroinoue6 <99115599+KeitaroInoue66@users.noreply.github.com>
2022-07-29 19:00:04 +0900

    Bookモデルのコールバックを作成、booksテーブルに販売状況のステータスカラムを追加

--- app/models/book.rb
+++ app/models/book.rb
@@ -9,12 +9,30 @@ class Book < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 25 }
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-
+  
+  # 本の名前に"Dog"が含まれていたら"Dog Cute"という文字列に置き換えるコールバック
   before_validation :add_Cute_to_dog
 
   def add_Cute_to_dog
+    # gsubメソッドはマッチした全ての部分を特定の文字列に置換します
     self.name = self.name.gsub(/Dog/) do |matched|
         "Dog Cute #{matched}"
     end
   end
+
+  # Bookモデルを削除した際に削除されたデータの内容をログに書き込む処理が動作するようにするコールバック
+  after_destroy do
+    Rails.logger.info "Book is deleted: #{self.attributes}"
+  end
+
+  # 価格が5000円以下のBookモデルをdestroyした場合に、警告のログが吐き出されるようにするコールバック
+  after_destroy :if => :high_price? do
+    Rails.logger.warn "Book with high price is deleted: #{self.attributes}"
+    Rails.logger.warn "価格が超えています！"
+  end
+
+  def high_price?
+    price >= 5000
+  end
+  
 end
