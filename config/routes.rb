c89a34472fe06787bb046161818d52c9ff6a6a6f
keitaroinoue6 <99115599+KeitaroInoue66@users.noreply.github.com>
2022-08-03 10:46:49 +0900

    showアクションの追加、destroyアクションのルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # /books/:idというパターンにマッチするパスへアクセスした時にBooksコントローラーのshowアクションを実行する
   get "/books/:id" => "books#show"
+  # 書籍情報を削除するアクション
+  get "/books/:id" => "books#destroy"
 end
