2fe27ed39a141d80edbc03ee3b886eab0c1687d2
keitaroinoue6 <99115599+KeitaroInoue66@users.noreply.github.com>
2022-09-03 11:31:37 +0900

    UserモデルとProfileコントローラの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'profiles/show'
+  get 'profiles/edit'
+  get 'profiles/update'
   # /books/:idというパターンにマッチするパスへアクセスした時にBooksコントローラーのshowアクションを実行する
   get "/books/:id" => "books#show"
   # 書籍情報を削除するアクション
   get "/books/:id" => "books#destroy"
+  # scaffoldで作成するようなCRUD操作は下記の1行で済む
+  resources :publishers
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_29_095744) do
+ActiveRecord::Schema.define(version: 2022_09_03_022952) do
 
   create_table "authors", force: :cascade do |t|
     t.string "name"
@@ -42,6 +42,14 @@ ActiveRecord::Schema.define(version: 2022_07_29_095744) do
   create_table "publishers", force: :cascade do |t|
     t.string "name"
     t.text "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "password"
+    t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
