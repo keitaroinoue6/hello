2430d8cfc06ea105d2611e634c57589c999deede
keitaroinoue6 <99115599+KeitaroInoue66@users.noreply.github.com>
2022-09-10 20:59:02 +0900

    profilecontrollerの作成、updateアクションの追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,5 +6,15 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    user = current_user
+    # params[:user] => {name: "bob", email: "bob@example.com"}
+    user.update(user_params)
+  end
+
+  private
+  # 外部から渡されたパラメーターをそのまま使用せずに
+  # 明示的に許可したキーのみを利用する
+  def user_params
+    params.require(:user).permit(:name, :email)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  get 'profiles/show'
-  get 'profiles/edit'
-  get 'profiles/update'
   # /books/:idというパターンにマッチするパスへアクセスした時にBooksコントローラーのshowアクションを実行する
   get "/books/:id" => "books#show"
   # 書籍情報を削除するアクション
-  get "/books/:id" => "books#destroy"
+  delete "/books/:id" => "books#destroy"
   # scaffoldで作成するようなCRUD操作は下記の1行で済む
   resources :publishers
+  # onlyを指定することで特定のルーティングのみを生成することができる
+  resource :profile, only: [:show, :edit, :update]
 end
